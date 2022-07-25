56bbdb9afb63b9a770204ec2d3199aa4c29ca99e
Magnus Anderssen <magnus.anderssen@apwide.com>
2022-07-25 12:06:45 +0200

    --wip--

--- src/components/InlineDialog/InlineDialog.vue
+++ src/components/InlineDialog/InlineDialog.vue
@@ -1,16 +1,17 @@
 <template>
     <div ref="dialog-container" class="kit-dialog-container">
-        <slot :is-open="open" :toggle="onTriggerClick" name="trigger" />
-        <Popup
-            ref="dialog"
-            :boundaries-element="boundariesElement"
+        <slot
             :is-open="open"
+            :toggle="onTriggerClick"
+            name="trigger"/>
+        <Popup
+            ref="dialog" :boundaries-element="boundariesElement" :is-open="open"
             :offset="offset"
             :placement="placement"
             :position-fixed="positionFixed"
             :target-element="$refs['dialog-container']">
             <div ref="dialog-content" class="kit-dialog-content">
-                <slot />
+                <slot/>
             </div>
         </Popup>
     </div>
@@ -60,8 +61,13 @@
                     document.addEventListener('click', this.onOutsideClick);
                     document.addEventListener('keydown', this.onKeyDown);
                     this.$emit('open');
+                    // eslint-disable-next-line no-underscore-dangle
+                    const openEvent = new Event('apw::inlinedialog-open');
+                    document.dispatchEvent(openEvent);
+                    document.addEventListener('apw::inlinedialog-open', this.onOtherInlineDialogOpen);
                 } else {
                     document.removeEventListener('click', this.onOutsideClick);
+                    document.removeEventListener('apw::inlinedialog-open', this.onOtherInlineDialogOpen);
                     document.removeEventListener('keydown', this.onKeyDown);
                     this.$emit('close');
                 }
@@ -69,6 +75,8 @@
         },
         beforeDestroy() {
             document.removeEventListener('click', this.onOutsideClick);
+            document.removeEventListener('apw::inlinedialog-open', this.onOtherInlineDialogOpen);
+            document.removeEventListener('keydown', this.onKeyDown);
         },
         methods: {
             onTriggerClick() {
@@ -84,20 +92,23 @@
                 if (event.keyCode === 27 && this.closeOnEsc) {
                     this.open = false;
                 }
+            },
+            onOtherInlineDialogOpen() {
+                this.open = false;
             }
         }
     };
 </script>
 
 <style scoped>
-    .kit-dialog-container {
-        display: inline-block;
-    }
+.kit-dialog-container {
+  display: inline-block;
+}
 
-    .kit-dialog-content {
-        background-color: white;
-        max-width: 300px;
-        max-height: 400px;
-        overflow: auto;
-    }
+.kit-dialog-content {
+  background-color: white;
+  max-width: 300px;
+  max-height: 400px;
+  overflow: auto;
+}
 </style>
