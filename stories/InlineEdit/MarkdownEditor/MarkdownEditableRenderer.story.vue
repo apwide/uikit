6bf485ffef7ac7713cb9a243d365e802ddd256f3
Magnus Anderssen <magnus.anderssen@apwide.com>
2023-07-03 15:33:00 +0200

    feat: BREAKING replace RichTextEditor with MarkdownEditor

---
+++ stories/InlineEdit/MarkdownEditor/MarkdownEditableRenderer.story.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h5>Editable - callback with error:</h5>
+    <KitMarkdownEditableRenderer allow-blur-to-save :value="text" @save-requested="onSaveError" />
+
+    <h5>Editable:</h5>
+    <KitMarkdownEditableRenderer :value="text" allow-blur-to-save @save-requested="onSaveRequested" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import KitMarkdownEditableRenderer from '../../../src/components/field-renderers/KitMarkdownEditableRenderer.vue'
+
+const text = ref(`# Description
+
+this environment serves as a demo for our main application Rotozor 3000.
+
+## Account
+
+Email [Bob](mailto:bob@rotozor-corp.com) to get a test account.
+
+## Features
+
+* Best in class generator
+* Super amazing suspense manager
+* Very nice
+
+[Report issues here](https://support.rotozor-corp.com/demo)
+`)
+
+function onSaveRequested(content, callback) {
+  text.value = content
+  callback()
+}
+
+function onSaveError(value, callback) {
+  setTimeout(() => callback(new Error('Something went wrong')), 100)
+}
+</script>
+
+<style scoped></style>
---
+++ stories/MarkdownEditor/MarkdownEditor.story.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Editable</h2>
+    <MarkdownEditor v-model="text" />
+    <Button appearance="primary" @click="save">Save</Button>
+
+    <h2>Readonly</h2>
+    <MarkdownEditor readonly v-model="text" />
+    <h2>Few content</h2>
+    <MarkdownEditor readonly value="This is a test" />
+    <Button appearance="primary" @click="save">Save</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import MarkdownEditor from '../../src/components/MarkdownEditor/KitMarkdownEditor.vue'
+import Button from '../../src/components/Button/Button.vue'
+
+const text = ref(`# Description
+
+this environment serves as a demo for our main application Rotozor 3000.
+
+## Account
+
+Email [Bob](mailto:bob@rotozor-corp.com) to get a test account.
+
+## Features
+
+* Best in class generator
+* Super amazing suspense manager
+* Very nice
+
+[Report issues here](https://support.rotozor-corp.com/demo)
+`)
+
+function save() {
+  alert(text.value)
+}
+</script>
