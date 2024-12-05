9accbd98a24cd653702ce1bcbe2c2464a9cc8680
Julien <julien@apwide.com>
2024-12-05 17:00:50 +0100

    chore(MultiSelectEditableRenderer): migrate to script setup syntax

--- src/components/field-renderers/MultiSelectEditableRenderer.vue
+++ src/components/field-renderers/MultiSelectEditableRenderer.vue
@@ -6,55 +6,55 @@
     :placement="placement"
     :force-is-editing="forceIsEditing"
     :hide-confirm-buttons="!confirm"
-    @start-editing="$emit('start-editing')"
-    @stop-editing="$emit('stop-editing')"
+    @start-editing="emit('start-editing')"
+    @stop-editing="emit('stop-editing')"
     @save-requested="onSaveRequested">
-    <Select
-      slot="editor"
-      slot-scope="props"
-      :value="props.value"
-      :options="allowedValues"
-      :open-on-focus="true"
-      :is-invalid="props.isInvalid"
-      :is-focused="props.isFocused"
-      :is-loading="props.isLoading"
-      :is-fetching="isFetching"
-      :placeholder="placeholder"
-      :no-options-message="noOptionsMessage"
-      :append-to-body="appendToBody"
-      :normalizer="normalizer"
-      :is-clearable="isClearable"
-      style="flex: 1"
-      :fixed-select-width="fixedSelectWidth"
-      :multi="true"
-      :async="async"
-      :confirm="confirm"
-      :keep-open-on-select="keepOpenOnSelect"
-      @input="
+    <template #editor="props">
+      <Select
+          :value="props.value"
+          :options="allowedValues"
+          :open-on-focus="true"
+          :is-invalid="props.isInvalid"
+          :is-focused="props.isFocused"
+          :is-loading="props.isLoading"
+          :is-fetching="isFetching"
+          :placeholder="placeholder"
+          :no-options-message="noOptionsMessage"
+          :append-to-body="appendToBody"
+          :normalizer="normalizer"
+          :is-clearable="isClearable"
+          style="flex: 1"
+          :fixed-select-width="fixedSelectWidth"
+          :multi="true"
+          :async="async"
+          :confirm="confirm"
+          :keep-open-on-select="keepOpenOnSelect"
+          @input="
         props.input($event)
-        $emit('input', $event)
+        emit('input', $event)
       "
-      @search-change="$emit('search-change', $event)"
-      @blur="
+          @search-change="emit('search-change', $event)"
+          @blur="
         props.blur($event)
-        $emit('blur', $event)
+        emit('blur', $event)
       "
-      @confirm="props.confirm"
-      @focus="props.focus"
-      @cancel="props.cancel">
-      <template v-if="$scopedSlots.tag" #tag="{ tag }">
-        <slot name="tag" :tag="tag" />
-      </template>
-      <template #option="{ option, isCurrent }">
-        <slot name="option" :option="option" :isCurrent="isCurrent" />
-      </template>
-      <template #custom-action>
-        <slot name="custom-action" />
-      </template>
-      <template #icon>
-        <slot name="icon" />
-      </template>
-    </Select>
+          @confirm="props.confirm"
+          @focus="props.focus"
+          @cancel="props.cancel">
+        <template v-if="$scopedSlots.tag" #tag="{ tag }">
+          <slot name="tag" :tag="tag" />
+        </template>
+        <template #option="{ option, isCurrent }">
+          <slot name="option" :option="option" :isCurrent="isCurrent" />
+        </template>
+        <template #custom-action>
+          <slot name="custom-action" />
+        </template>
+        <template #icon>
+          <slot name="icon" />
+        </template>
+      </Select>
+    </template>
     <slot>
       <MultiSelectRenderer :selected-values="selectedValueLabels" />
     </slot>
@@ -66,93 +66,69 @@
   </div>
 </template>
 
-<script>
+<script lang="ts" setup>
 import Select from '../Select/KitSelect.vue'
 import InlineEdit from '../Form/InlineEdit'
 import MultiSelectRenderer from './MultiSelectRenderer'
+import {ConfirmationCallback, Normalizer} from '@components/Select/types'
+import {computed} from 'vue'
 
-export default {
-  name: 'KitMultiSelectEditableRenderer',
-  components: { MultiSelectRenderer, InlineEdit, Select },
-  props: {
-    value: {
-      type: Array,
-      default: () => []
-    },
-    editable: {
-      type: Boolean,
-      default: true
-    },
-    placement: {
-      type: String,
-      default: 'right'
-    },
-    allowedValues: {
-      type: Array,
-      default: () => []
-    },
-    placeholder: {
-      type: String,
-      default: ''
-    },
-    noOptionsMessage: {
-      type: String,
-      default: 'No options'
-    },
-    isClearable: {
-      type: Boolean
-    },
-    isFetching: {
-      type: Boolean
-    },
-    async: {
-      type: Boolean
-    },
-    forceIsEditing: {
-      type: Boolean,
-      default: false
-    },
-    appendToBody: {
-      type: Boolean,
-      default: false
-    },
-    confirm: {
-      type: Boolean,
-      default: true
-    },
-    normalizer: {
-      type: Function,
-      default: (value) => ({
-        id: value,
-        value,
-        label: value,
-        disabled: false
-      })
-    },
-    fixedSelectWidth: {
-      type: String
-    },
-    keepOpenOnSelect: {
-      type: Boolean
+type Props = {
+  value?: Array
+  editable?: boolean
+  placement?: string
+  allowedValues?: Array
+  placeholder?: string
+  noOptionsMessage?: string
+  isClearable?: boolean
+  isFetching?: boolean
+  async?: boolean
+  forceIsEditing?: boolean
+  appendToBody?: boolean
+  confirm?: boolean
+  normalizer?: Normalizer<unknown>
+  fixedSelectWidth?: string
+  keepOpenOnSelect?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: () => [],
+  editable: true,
+  placement: 'right',
+  allowedValues: () => [],
+  placeholder: '',
+  noOptionsMessage: 'No options',
+  forceIsEditing: false,
+  appendToBody: false,
+  confirm: true,
+  normalizer: () => (value) => ({
+    id: value,
+    value,
+    label: value,
+    disabled: false
+  }),
+})
+
+const emit = defineEmits<{
+  (event: 'save-requested', value: Array, callback: ConfirmationCallback<Array>)
+  (event: 'start-editing')
+  (event: 'stop-editing')
+  (event: 'input', value: Array)
+  (event: 'search-change', searchTerm: string)
+  (event: 'blur', e: FocusEvent)
+}>()
+
+const selectedValueLabels = computed(() => props.value.map(props.normalizer).map(({ label }) => label))
+
+function onSaveRequested(value, callback) {
+  try {
+    if (Array.isArray(value)) {
+      emit('save-requested', value, callback)
+    } else {
+      callback(new Error('Value should be an array'))
     }
-  },
-  computed: {
-    selectedValueLabels() {
-      return this.value.map(this.normalizer).map(({ label }) => label)
-    }
-  },
-  methods: {
-    onSaveRequested(value, callback) {
-      try {
-        if (Array.isArray(value)) {
-          this.$emit('save-requested', value, callback)
-        } else {
-          callback(new Error('Value should be an array'))
-        }
-      } catch (error) {
-        callback(error)
-      }
-    }
+  } catch (error) {
+    callback(error)
   }
 }
 </script>
