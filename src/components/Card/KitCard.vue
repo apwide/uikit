75f56d50fd465c74c3e5554ac03286d7aa55c814
Julien <julien@apwide.com>
2025-03-18 15:03:03 +0100

    feat(dark-mode): KitCard

--- src/components/Card/KitCard.vue
+++ src/components/Card/KitCard.vue
@@ -13,24 +13,39 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 <style scoped>
 .kit-card {
+  --kit-card-bg-color: var(--kit-page-bg-color);
+  --kit-card-border-color: #e0e2e6;
+  --kit-card-shadow1-color: rgba(9, 30, 66, 0.31);
+  --kit-card-shadow2-color: var(--kit-card-shadow1-color);
+}
+.kit-dark .kit-card {
+  --kit-card-bg-color: #282E33;
+  --kit-card-border-color: #313a41;
+  --kit-card-shadow1-color: #39424a;
+  --kit-card-shadow2-color: #0304045c;
+}
+.kit-card {
   border-radius: 3px;
-  background-color: white;
+  background-color: var(--kit-card-bg-color);
   padding: 10px;
+}
+.kit-dark .kit-card {
+  border: 1px solid var(--kit-card-border-color);
 }
 .kit-card[elevation='1'] {
   box-shadow: none;
-  border: 1px solid #e0e2e6;
+  border: 1px solid var(--kit-card-border-color);
 }
 .kit-card[elevation='2'] {
-  box-shadow: 0 2px 6px -2px rgba(9, 30, 66, 0.31), 0 0 1px rgba(9, 30, 66, 0.31);
+  box-shadow: 0 2px 6px -2px var(--kit-card-shadow1-color), 0 0 1px var(--kit-card-shadow2-color);
 }
 .kit-card[elevation='3'] {
-  box-shadow: 0 4px 8px -2px rgba(9, 30, 66, 0.31), 0 0 1px rgba(9, 30, 66, 0.31);
+  box-shadow: 0 4px 8px -2px var(--kit-card-shadow1-color), 0 0 1px var(--kit-card-shadow2-color);
 }
 .kit-card[elevation='4'] {
-  box-shadow: 0 6px 10px -2px rgba(9, 30, 66, 0.31), 0 0 1px rgba(9, 30, 66, 0.31);
+  box-shadow: 0 6px 10px -2px var(--kit-card-shadow1-color), 0 0 1px var(--kit-card-shadow2-color);
 }
 .kit-card[elevation='5'] {
-  box-shadow: 0 8px 12px -2px rgba(9, 30, 66, 0.31), 0 0 1px rgba(9, 30, 66, 0.31);
+  box-shadow: 0 8px 12px -2px var(--kit-card-shadow1-color), 0 0 1px var(--kit-card-shadow2-color);
 }
 </style>
