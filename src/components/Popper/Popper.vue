dc8c57aae2a687bd914633bac3548492aa2fbc30
Julien <julien@apwide.com>
2025-03-24 15:30:47 +0100

    chore(Popper): migrated to setup/TS

--- src/components/Popper/Popper.vue
+++ src/components/Popper/Popper.vue
@@ -1,114 +1,103 @@
-<script>
-import { createPopper } from '@popperjs/core'
-
+<script lang="ts">
 export default {
-  name: 'KitPopper',
-  props: {
-    targetElement: {
-      type: HTMLElement,
-      required: true
-    },
-    placement: {
-      type: String,
-      default: 'bottom-end',
-      validator(value) {
-        return [
-          'auto',
-          'auto-start',
-          'auto-end',
-          'top',
-          'top-start',
-          'top-end',
-          'bottom',
-          'bottom-start',
-          'bottom-end',
-          'right',
-          'right-start',
-          'right-end',
-          'left',
-          'left-start',
-          'left-end'
-        ].includes(value)
-      }
-    },
-    offset: {
-      type: [Array, String],
-      default: () => [0, 5]
-    },
-    transitionDelay: {
-      type: Number,
-      default: 0
-    },
-    boundariesElement: {
-      type: [String, HTMLElement, Function],
-      default: 'viewport'
-    },
-    positionFixed: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data() {
-    return {
-      contentWidth: 0,
-      popper: null,
-      interval: null
-    }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.initPopper()
-    })
-
-    this.interval = setInterval(this.updateBasedOnContent, 250)
-  },
-  beforeDestroy() {
-    clearInterval(this.interval)
-    setTimeout(() => this.popper.destroy(), this.transitionDelay)
-  },
-  methods: {
-    initPopper() {
-      const [defaultSlot] = this.$slots.default
-      const boundariesElement =
-        typeof this.boundariesElement === 'function' ? this.boundariesElement() : this.boundariesElement
-
-      this.popper = createPopper(this.targetElement, defaultSlot.elm, {
-        placement: this.placement,
-        positionFixed: this.positionFixed,
-        modifiers: [
-          {
-            name: 'offset',
-            options: { offset: this.offset }
-          },
-          {
-            name: 'preventOverflow',
-            options: { boundary: boundariesElement }
-          }
-        ]
-      })
-    },
-    updateBasedOnContent() {
-      try {
-        const [defaultSlot] = this.$slots.default
-        const contentWidth = defaultSlot.elm.getBoundingClientRect().width
-
-        if (contentWidth !== this.contentWidth) {
-          this.contentWidth = contentWidth
-          this.popper.update()
-        }
-      } catch (e) {
-        // ignore
-      }
-    },
-    update() {
-      if (this.popper) {
-        this.popper.update()
-      }
-    }
-  },
   render() {
     const [defaultSlot] = this.$slots.default
     return defaultSlot
   }
 }
+</script>
+<script setup lang="ts">
+import { createPopper, type Instance } from '@popperjs/core'
+import { getCurrentInstance, nextTick, onBeforeUnmount, onMounted, ref } from 'vue'
+
+type Props = {
+  targetElement: HTMLElement
+  placement?: 'auto'
+    | 'auto-start'
+    | 'auto-end'
+    | 'top'
+    | 'top-start'
+    | 'top-end'
+    | 'bottom'
+    | 'bottom-start'
+    | 'bottom-end'
+    | 'right'
+    | 'right-start'
+    | 'right-end'
+    | 'left'
+    | 'left-start'
+    | 'left-end'
+  offset?: number[] | string[]
+  transitionDelay?: number
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  boundariesElement?: string | HTMLElement | Function
+  positionFixed?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  placement: 'bottom-end',
+  offset: () => [0, 5],
+  transitionDelay: 0,
+  boundariesElement: 'viewport',
+  positionFixed: false
+})
+
+const contentWidth = ref(0)
+const popper = ref<Instance>()
+const interval = ref<number>()
+
+onMounted(() => {
+  nextTick(() => {
+    initPopper()
+  })
+  interval.value = setInterval(updateBasedOnContent, 250)
+})
+
+onBeforeUnmount(() => {
+  clearInterval(interval.value)
+  setTimeout(() => popper.value.destroy(), props.transitionDelay)
+})
+
+
+const instance = getCurrentInstance()
+
+function initPopper() {
+  const [defaultSlot] = instance.proxy.$slots.default
+  const boundariesElement =
+    typeof props.boundariesElement === 'function' ? props.boundariesElement() : props.boundariesElement
+
+  popper.value = createPopper(props.targetElement, defaultSlot.elm, {
+    placement: props.placement,
+    positionFixed: props.positionFixed,
+    modifiers: [
+      {
+        name: 'offset',
+        options: { offset: props.offset }
+      },
+      {
+        name: 'preventOverflow',
+        options: { boundary: boundariesElement }
+      }
+    ]
+  })
+}
+
+function updateBasedOnContent() {
+  try {
+    const [defaultSlot] = instance.proxy.$slots.default
+    const currentContentWidth = defaultSlot.elm.getBoundingClientRect().width
+
+    if (currentContentWidth !== contentWidth.value) {
+      contentWidth.value = currentContentWidth
+      popper.value.update()
+    }
+  } catch (e) {
+    // ignore
+  }
+}
+
+function update() {
+  popper.value?.update()
+}
+
 </script>
